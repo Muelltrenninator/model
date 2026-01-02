55ca9580d772ccd53d27a058ddb0cb019ffb4235
EinfachnurJuls <hack.juls@gmail.com>
2026-01-02 19:59:37 +0100

    hardcoded path entfernt

--- README.md
+++
@@ -1,1 +0,0 @@
-An image classification AI/ ML model intended for classifying and sorting trash
---
+++ muelltrenninator/interface.py
@@ -0,0 +1,24 @@
+import gradio as gr 
+import os
+
+from train_model import load_model, evalute_input
+from model_architecture import neural_network
+
+
+def predict(input):
+    curr_model = load_model(os.path.dirname(os.path.realpath(__file__))+ "/trained_models/model.pth")
+    predicted = evalute_input(curr_model,input)
+
+
+    return predicted
+
+demo = gr.Interface(
+    fn= predict,
+    inputs = gr.Image(type = "filepath"),
+    outputs= gr.Textbox(),
+    flagging_mode = "never"
+
+
+)
+
+demo.launch()
